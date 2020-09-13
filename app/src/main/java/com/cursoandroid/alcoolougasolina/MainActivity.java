8ae88f0b304c6691c0c7ad3504ac886d27ffe848
Roberta Cecilia de Carvalho <roberta.carvalho@wkm.com.br>
2020-09-12 23:10:39 -0300

    Criar logica de calculo que verifica se é melhor alcool ou gasolina
    Criar logica de validacao dos campos de valores de preco de alcool e gasolina
    
    Exibir mensagem de validacao ou mensagem de resultado do calculo de verificacao no componente TextView do resultado

--- app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/cursoandroid/alcoolougasolina/MainActivity.java
@@ -4,17 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputEditText editPrecoGasolina, editPrecoAlcool;
+    Button btnCalcular;
+    TextView campoResultado;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
+        editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        campoResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularPrecos(View view)
+    {
+        String precoAlcool = editPrecoAlcool.getText().toString();
+        String precoGasolina = editPrecoGasolina.getText().toString();
+
+        // validar campos
+        boolean camposValidados = validarCampos(precoAlcool, precoGasolina);
+        if ( camposValidados ) {
+            // realizar cálculo
+            Double valorAlcool = Double.parseDouble(precoAlcool);
+            Double valorGasolina = Double.parseDouble(precoGasolina);
+
+            String resultado = valorAlcool / valorGasolina >= 0.7 ? "Gasolina" : "Álcool";
+
+            campoResultado.setText("É melhor usar " + resultado);
+
+        } else {
+            campoResultado.setText("Informes valores de preço válidos para álcool ou gasolina.");
+        }
+    }
+
+    private boolean validarCampos(String precoGasolina, String precoAlcool)
+    {
+        boolean estaoValidos = true;
+
+        if (precoAlcool == null || precoAlcool.equals("") )
+            estaoValidos = false;
+
+        if (precoGasolina == null || precoGasolina.equals("") )
+            estaoValidos = false;
+
+        return estaoValidos;
     }
 
 }
